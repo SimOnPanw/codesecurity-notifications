8d71ff37fbb40a6e946d252cc7fd012dfaac0cb9
Simon Melotte <smelotte@paloaltonetworks.com>
2023-06-30 23:23:32 +0200

    fix with flake8

--- README.md
+++ README.md
@@ -1,13 +1,11 @@
 # How to use this script
 
-## Authentication
+## Set your environment variables
 
 Create an access key from Settings then Access key  
-Get the path to console from Compute tab, System, Utilities  
 
-Create a file into home directory .prismacloud/credentials.json with the following structure  
 
-```json
+```
 export PRISMA_API_URL="__REDACTED__"
 export PRISMA_ACCESS_KEY="__REDACTED__"
 export PRISMA_SECRET_KEY="__REDACTED__"
@@ -16,5 +14,5 @@ export PRISMA_SECRET_KEY="__REDACTED__"
 ## Run the script
 
 ```console
-python3 code_security.py
+python3 add_notifications.py -r repo1 repo2 --severity <SEVERITY> --integration-name <INTEGRATION_NAME>
 ```
--- add_notifications.py
+++ add_notifications.py
@@ -1,17 +1,15 @@
 __author__ = "Simon Melotte"
 
 import os
-import requests
-import argparse
 import json
-from datetime import datetime
+import argparse
+import requests
 
 
 def get_code_security_notification(base_url, token):
     try:
         url = f"https://{base_url}/code/api/v1/vcs/settings/scheme?featureType=pcNotifications"
-        headers = {"content-type": "application/json; charset=UTF-8",
-                   'Authorization': f'Bearer {token}'}
+        headers = {"content-type": "application/json; charset=UTF-8", "Authorization": f"Bearer {token}"}
         response = requests.get(url, headers=headers)
         data = response.json()
         for section in data.get("sections", []):
@@ -32,12 +30,9 @@ def get_code_security_notification(base_url, token):
 def set_code_security_notification(base_url, token, existing_notifications):
     try:
         url = f"https://{base_url}/code/api/v1/vcs/settings/scheme"
-        headers = {"content-type": "application/json; charset=UTF-8",
-                   'Authorization': f'Bearer {token}'}
-        parameters = {"scheme": {"pcNotifications": existing_notifications},
-                      "type": "pcNotifications"}
-        response = requests.post(
-            url, headers=headers, data=json.dumps(parameters))
+        headers = {"content-type": "application/json; charset=UTF-8", "Authorization": f"Bearer {token}"}
+        parameters = {"scheme": {"pcNotifications": existing_notifications}, "type": "pcNotifications"}
+        response = requests.post(url, headers=headers, data=json.dumps(parameters))
         print(f"Response: {response}")
     except Exception as e:
         print(f"Error in set_code_security_notification: {e}")
@@ -51,11 +46,9 @@ def add_section(existing_notifications, repos, severity_level, integration_id, i
                 "severityLevel": severity_level,
                 "excludePolicies": [],
                 "securityCategories": [],
-                "pcNotificationIntegrations": [
-                    {"integrationId": integration_id}
-                ]
+                "pcNotificationIntegrations": [{"integrationId": integration_id}],
             },
-            "isDefault": is_default
+            "isDefault": is_default,
         }
         if existing_notifications and "sections" in existing_notifications:
             existing_notifications["sections"].append(new_section)
@@ -68,7 +61,7 @@ def add_section(existing_notifications, repos, severity_level, integration_id, i
 def get_prisma_id(base_url, token):
     try:
         url = f"https://{base_url}/license"
-        headers = {'x-redlock-auth': token}
+        headers = {"x-redlock-auth": token}
         response = requests.get(url, headers=headers)
         data = response.json()
         return data.get("prismaId")
@@ -80,16 +73,14 @@ def get_prisma_id(base_url, token):
 def get_integration_id(base_url, token, prisma_id, integration_name):
     try:
         url = f"https://{base_url}/api/v1/tenant/{prisma_id}/integration"
-        headers = {"content-type": "application/json; charset=UTF-8",
-                   'Authorization': 'Bearer ' + token}
+        headers = {"content-type": "application/json; charset=UTF-8", "Authorization": "Bearer " + token}
         response = requests.get(url, headers=headers)
         data = response.json()
         for integration in data:
             if integration.get("name") == integration_name:
                 return integration.get("id")
         # If the integration name is not found, raise an exception
-        raise ValueError(
-            f"Integration with name {integration_name} not found.")
+        raise ValueError(f"Integration with name {integration_name} not found.")
     except Exception as e:
         # Re-raise the exception to be handled by the caller
         raise Exception(f"Error in get_integration_id: {e}")
@@ -98,10 +89,7 @@ def get_integration_id(base_url, token, prisma_id, integration_name):
 def login_saas(base_url, access_key, secret_key):
     try:
         url = f"https://{base_url}/login"
-        payload = json.dumps({
-            "username": access_key,
-            "password": secret_key
-        })
+        payload = json.dumps({"username": access_key, "password": secret_key})
         headers = {"content-type": "application/json; charset=UTF-8"}
         response = requests.post(url, headers=headers, data=payload)
         return response.json().get("token")
@@ -112,13 +100,12 @@ def login_saas(base_url, access_key, secret_key):
 
 def main(repositories, severity_level, integration_name):
     try:
-        url = os.environ.get('PRISMA_API_URL')
-        identity = os.environ.get('PRISMA_ACCESS_KEY')
-        secret = os.environ.get('PRISMA_SECRET_KEY')
+        url = os.environ.get("PRISMA_API_URL")
+        identity = os.environ.get("PRISMA_ACCESS_KEY")
+        secret = os.environ.get("PRISMA_SECRET_KEY")
 
         if not url or not identity or not secret:
-            print(
-                "Error: PRISMA_API_URL, PRISMA_ACCESS_KEY or PRISMA_SECRET_KEY environment variables are not set.")
+            print("Error: PRISMA_API_URL, PRISMA_ACCESS_KEY or PRISMA_SECRET_KEY environment variables are not set.")
             return
 
         token = login_saas(url, identity, secret)
@@ -128,18 +115,15 @@ def main(repositories, severity_level, integration_name):
             return
 
         existing_notifications = get_code_security_notification(url, token)
-        
+
         prisma_id = get_prisma_id(url, token)
         try:
-            integration_id = get_integration_id(
-                url, token, prisma_id, integration_name)
+            integration_id = get_integration_id(url, token, prisma_id, integration_name)
         except Exception as e:
             print(e)
             return
 
-
-        existing_notifications = add_section(
-            existing_notifications, repositories, severity_level, integration_id, False)
+        existing_notifications = add_section(existing_notifications, repositories, severity_level, integration_id, False)
         set_code_security_notification(url, token, existing_notifications)
 
     except Exception as e:
@@ -148,12 +132,11 @@ def main(repositories, severity_level, integration_name):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Configure Prisma Cloud")
-    parser.add_argument("-r", "--repositories", nargs='+', required=True,
-                        help="List of repositories")
-    parser.add_argument("-s", "--severity", required=True, choices=[
-                        'CRITICAL', 'HIGH', 'MEDIUM', 'LOW', 'INFO'], help="Severity level")
-    parser.add_argument("-i", "--integration-name",
-                        required=True, help="Integration name")
+    parser.add_argument("-r", "--repositories", nargs="+", required=True, help="List of repositories")
+    parser.add_argument(
+        "-s", "--severity", required=True, choices=["CRITICAL", "HIGH", "MEDIUM", "LOW", "INFO"], help="Severity level"
+    )
+    parser.add_argument("-i", "--integration-name", required=True, help="Integration name")
     args = parser.parse_args()
 
     main(args.repositories, args.severity, args.integration_name)
